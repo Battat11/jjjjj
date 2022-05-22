c56e592fe3a18142bdfd95b9daf92e344520227c
Battat11 <106020568+Battat11@users.noreply.github.com>
2022-05-22 16:30:57 +0300

    Add files via upload

---
+++ WpfApp3/WpfApp3/Animal.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+namespace шещп
+{
+    class Animal
+    {
+        protected int width = 0, height = 0;
+        protected SolidColorBrush color = new SolidColorBrush(Colors.Red);
+        protected Rectangle body = new Rectangle();
+        protected int left = 0, top = 0, MoveLeft = 0, MoveTop = 0, speed = 0;
+        public void Copy(Animal ob)
+        {
+            this.width = ob.GetInfo()[0];
+            this.height = ob.GetInfo()[1];
+            this.left = ob.GetInfo()[2];
+            this.top = ob.GetInfo()[3];
+            this.speed = ob.GetInfo()[4];
+        }
+        public int[] GetInfo()
+        {
+            int[] a = new int[5];
+            a[0] = this.width;
+            a[1] = this.height;
+            a[2] = this.left;
+            a[3] = this.top;
+            a[4] = this.speed;
+            return a;
+        }
+        public Animal(int w, int h, int l, int t, int s = 1)
+        {
+            width = w;
+            height = h;
+            left = l;
+            top = t;
+            speed = s;
+        }
+        public void SetColor(SolidColorBrush c)
+        {
+            this.color = c;
+        }
+        public void Translaiet(int a, int b)
+        {
+            this.left += a;
+            this.top += b;
+            this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+        }
+        public void Load()
+        {
+            this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+            this.body.Width = this.width;
+            this.body.Height = this.height;
+            this.body.Fill = this.color;
+        }
+        public void Update()
+        {
+            if (this.MoveLeft != this.left || this.MoveTop != this.top)
+            {
+                if (this.left < this.MoveLeft) { this.left += this.speed; }
+                if (this.left > this.MoveLeft) { this.left -= this.speed; }
+                if (this.top < this.MoveTop) { this.top += this.speed; }
+                if (this.top > this.MoveTop) { this.top -= this.speed; }
+                this.body.Margin = new System.Windows.Thickness(this.left, this.top, 0, 0);
+            }
+            if (this.MoveLeft == this.left && this.MoveTop == this.top)
+            {
+                Random rand = new Random();
+                this.MoveLeft = this.left + rand.Next(-50, 50);
+                this.MoveTop = this.top + rand.Next(-50, 50);
+            }
+        }
+        public Rectangle Get()
+        {
+            return this.body;
+        }
+    }
+}
---
+++ WpfApp3/WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace шещп
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Animal[] Animals = new Animal[100];
+        string MouseStatus = "CreateNewAnimal";
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow1_Activated(object sender, EventArgs e)
+        {
+            if (index > 1) { for (int i = 0; i < Animals.Length; i++) { Animals[i].Update(); } }
+        }
+
+        private void MainWindow1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (index < Animals.Length && MouseStatus == "CreateNewAnimal")
+            {
+                Random rand = new Random();
+                Animal[] temp = new Animal[Animals.Length + 1];
+                for (int i = 0; i < Animals.Length; i++)
+                {
+                    temp[i] = new Animal(0, 0, 0, 0);
+                    temp[i].Copy(Animals[i]);
+                }
+                Animals = new Animal[temp.Length];
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    Animals[i] = new Animal(0, 0, 0, 0);
+                    Animals[i].Copy(temp[i]);
+                }
+                SolidColorBrush color = new SolidColorBrush(Color.FromRgb(Convert.ToByte(rand.Next(1, 255)), Convert.ToByte(rand.Next(1, 255)), Convert.ToByte(rand.Next(1, 255))));
+                Animals[index] = new Animal(rand.Next(20, 40), rand.Next(10, 20), Convert.ToInt32(e.GetPosition(can).X), Convert.ToInt32(e.GetPosition(can).Y));
+                Animals[index].SetColor(color);
+                Animals[index].Load();
+                can.Children.Add(Animals[index].Get());
+                index++;
+            }
+
+        }
+
+        private void NewAnim_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "CreateNewAnimal";
+        }
+
+        private void NewTree_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "CreateNewTrees";
+        }
+
+        private void None_Click(object sender, RoutedEventArgs e)
+        {
+            MouseStatus = "none";
+        }
+    }
+}
